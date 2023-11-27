93120de35ab32343034f7c04da67280723ff20b7
ThaSMorato <tha.s.morato@gmail.com>
2023-11-27 20:10:47 -0300

    product styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,26 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
+import Image from "next/image"
 import { useRouter } from "next/router"
 
 const Product = () => {
   const { query } = useRouter()
-  return <h1>Product {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        {/* <Image > */}
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet itaque nemo a quibusdam unde repellendus, aperiam fuga. Quia quisquam, animi reprehenderit beatae, dolor sequi voluptatum unde, iure modi magnam corrupti.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
 
 export default Product
